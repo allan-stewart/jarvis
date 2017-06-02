8d23a3c737cfc437ed736f511aebb0e67047f01a
Allan Stewart <allan-stewart@pluralsight.com>
2017-06-02 12:12:07 -0600

    Reduce duplication in encoding

--- src/skills/encode.js
+++ src/skills/encode.js
@@ -4,24 +4,26 @@ const slackEncoder = require('../slack-encoder')
 module.exports = (controller) => {
 
   controller.hears([/encode base64 (.*)$/i,], ['direct_message','direct_mention'], (bot, message) => {
-    let encoded = Buffer.from(slackEncoder.decode(message.match[1])).toString('base64')
-    bot.reply(message, slackEncoder.encode(encoded))
+    transform(bot, message, message.match[1], x => Buffer.from(x).toString('base64'))
   })
 
   controller.hears([/decode base64 (.*)$/i,], ['direct_message','direct_mention'], (bot, message) => {
-    let decoded = Buffer.from(slackEncoder.decode(message.match[1]), 'base64').toString('utf8')
-    bot.reply(message, slackEncoder.encode(decoded))
+    transform(bot, message, message.match[1], (x) => Buffer.from(x, 'base64').toString('utf8'))
   })
 
   controller.hears([/encode url (.*)$/i,], ['direct_message','direct_mention'], (bot, message) => {
-    let encoded = encodeURIComponent(slackEncoder.decode(message.match[1]))
-    bot.reply(message, slackEncoder.encode(encoded))
+    transform(bot, message, message.match[1], encodeURIComponent)
   })
 
   controller.hears([/decode url (.*)$/i,], ['direct_message','direct_mention'], (bot, message) => {
-    let encoded = decodeURIComponent(slackEncoder.decode(message.match[1]))
-    bot.reply(message, slackEncoder.encode(encoded))
+    transform(bot, message, message.match[1], decodeURIComponent)
   })
 
   logger.info('Loaded slack-users skill')
+}
+
+const transform = (bot, message, match, transformer) => {
+  logger.info('transforming', match)
+  var result = transformer(slackEncoder.decode(match))
+  bot.reply(message, slackEncoder.encode(result))
 }
