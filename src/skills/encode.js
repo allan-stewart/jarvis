6d57b2b4e86b01b3713add12839121313dfd1202
Allan Stewart <allan-stewart@pluralsight.com>
2017-06-02 10:04:44 -0600

    Add url encoding/decoding and handle slack characters

--- src/skills/encode.js
+++ src/skills/encode.js
@@ -1,15 +1,26 @@
 const logger = require('../logger')
+const slackEncoder = require('../slack-encoder')
 
 module.exports = (controller) => {
 
   controller.hears([/encode base64 (.*)$/i,], ['direct_message','direct_mention'], (bot, message) => {
-    let encoded = Buffer.from(message.match[1]).toString('base64')
-    bot.reply(message, encoded)
+    let encoded = Buffer.from(slackEncoder.decode(message.match[1])).toString('base64')
+    bot.reply(message, slackEncoder.encode(encoded))
   })
 
   controller.hears([/decode base64 (.*)$/i,], ['direct_message','direct_mention'], (bot, message) => {
-    let decoded = Buffer.from(message.match[1], 'base64').toString('utf8')
-    bot.reply(message, decoded)
+    let decoded = Buffer.from(slackEncoder.decode(message.match[1]), 'base64').toString('utf8')
+    bot.reply(message, slackEncoder.encode(decoded))
+  })
+
+  controller.hears([/encode url (.*)$/i,], ['direct_message','direct_mention'], (bot, message) => {
+    let encoded = encodeURIComponent(slackEncoder.decode(message.match[1]))
+    bot.reply(message, slackEncoder.encode(encoded))
+  })
+
+  controller.hears([/decode url (.*)$/i,], ['direct_message','direct_mention'], (bot, message) => {
+    let encoded = decodeURIComponent(slackEncoder.decode(message.match[1]))
+    bot.reply(message, slackEncoder.encode(encoded))
   })
 
   logger.info('Loaded slack-users skill')
