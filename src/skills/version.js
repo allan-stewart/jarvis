cbdd3b7b39338194f9b9aec0e1afc819f9a627c8
Allan Stewart <allan-stewart@pluralsight.com>
2017-06-02 13:40:25 -0600

    Update version text

--- src/skills/version.js
+++ src/skills/version.js
@@ -1,14 +1,13 @@
 const logger = require('../logger')
 const version = require('../../package.json').version
+const admins = require('../admins')
 
 module.exports = (controller, skillData) => {
 
   controller.hears([/version/], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
-    logger.info(JSON.stringify(message))
-
-    bot.reply(message, {
-      text: `My version is currently: ${version}`
-    })
+    let admin = admins.getAdmin(message.user)
+    let text = admin ? `I am currently at ${version}, ${admin.honorific}.` : `I am currently at ${version}`
+    bot.reply(message, text)
   })
 
   skillData.publicCommand('get my current version: `version`')
